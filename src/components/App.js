b0160c8bcd1474f46169c4bd7a9d587c1dea882d
rockysantiago <raphael.santiago@icloud.com>
2018-07-26 13:44:20 +0800

    add search bar

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import SearchBar from './SearchBar';
+const API_KEY = 'AIzaSyDyxiE7Bi6UxAAGjvjQugbab6Yyl6s4k-o';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    );
+  }
+}
+
+export default App;
