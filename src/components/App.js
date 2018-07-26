105129034117a4e80b477c5122985e2816f013e8
rockysantiago <raphael.santiago@icloud.com>
2018-07-26 16:09:13 +0800

    add ajax requests with react

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,39 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
+import YTSearch from 'youtube-api-search';
 import SearchBar from './SearchBar';
-const API_KEY = 'AIzaSyDyxiE7Bi6UxAAGjvjQugbab6Yyl6s4k-o';
+import VideoDetail from './VideoDetail';
+import VideoList from './VideoList';
+
+// To test, replace the API_KEY with your own personal YouTube Data API key
+const API_KEY = '';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { videos: [], selectedVideo: null };
+    this.videoSearch('rick astley');
+  }
+
+  videoSearch(term) {
+    YTSearch({ key: API_KEY, term: term }, videos => {
+      this.setState({ videos: videos, selectedVideo: videos[0] });
+    });
+  }
+
   render() {
+    const videoSearch = _.debounce(term => {
+      this.videoSearch(term);
+    }, 1000);
+
     return (
       <div>
-        <SearchBar />
+        <SearchBar onSearchTermChange={videoSearch} />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList
+          onVideoSelect={selectedVideo => this.setState({ selectedVideo })}
+          videos={this.state.videos}
+        />
       </div>
     );
   }
